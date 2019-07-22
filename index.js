d3c4a2ec8cb6c9e37b4ce920b37e276343b2ff67
“Guilherme <guilhermebromonschenkel@gmail.com>
2019-07-22 00:08:52 -0300

    Decrypting script

--- index.js
+++ index.js
@@ -5,12 +5,22 @@ async function boot(){
     console.log('=> Checking if file exists')
     const isFileCreated = require('./services/file').checkFileExistance()
     
-    if(!isFileCreated){
+    if(isFileCreated){
+        console.log('Retrieving data from local...')
+        const data = JSON.parse(require('fs').readFileSync('answer.json'))
+
+        console.log('=> Decrypting data')
+        require('./services/crypt').decrypt(data.cifrado, data.numero_casas)
+    }
+    else{
         console.log('Retrieving data from server...')
         const data = await require('./services/api').getData()
 
         console.log('Saving data on JSON file...')
         require('./services/file').saveDataOnFile(data)
+
+        console.log('=> Decrypting data')
+        require('./services/crypt').decrypt(data.cifrado, data.numero_casas)
     }
 }
 
